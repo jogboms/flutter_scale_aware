f8bfab44b837b99a22517270bfaab7b3e2b795a4
Jogboms <jeremiahogbomo@gmail.com>
2019-11-08 17:37:22 +0100

    Include example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dpi_aware/flutter_dpi_aware.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Demo',
-      theme: ThemeData.dark(),
-      home: MyHomePage(title: 'Demo'),
+    return ScreenAwareProvider(
+      config: ScreenConfig(),
+      builder: (_, screen, __) => MaterialApp(
+        title: 'Demo',
+        theme: ThemeData.dark(),
+        home: MyHomePage(title: '${screen.size.width} x ${screen.size.height}'),
+      ),
+//      child: MaterialApp(
+//        title: 'Demo',
+//        theme: ThemeData.dark(),
+//        home: MyHomePage(title: '${Screen().size.width} x ${Screen().size.height}'),
+//      ),
     );
   }
 }
@@ -22,8 +31,54 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(title)),
-      body: Center(
-        child: Text('You have pushed the button this many times:'),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 16.dp),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 16.dp),
+              Container(
+                width: 200.dp,
+                height: 200.dp,
+                color: Colors.red,
+                child: Center(child: Text("200.dp \nx\n 200.dp", textAlign: TextAlign.center)),
+              ),
+              SizedBox(height: 16.dp),
+              Container(
+                width: 200.vw,
+                height: 200.vh,
+                color: Colors.blue,
+                child: Center(child: Text("200.vw \nx\n 200.vh", textAlign: TextAlign.center)),
+              ),
+              SizedBox(height: 16.dp),
+              Container(
+                width: 200,
+                height: 200,
+                color: Colors.green,
+                child: Center(child: Text("200 \nx\n 200", textAlign: TextAlign.center)),
+              ),
+              SizedBox(height: 16.dp),
+              Text(
+                'The button is fixed at 40',
+                style: TextStyle(fontSize: 40),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 16.dp),
+              Text(
+                'The button is flexible at 40.sp',
+                style: TextStyle(fontSize: 40.sp),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 16.dp),
+              Text(
+                Screen().toString(),
+                style: TextStyle(fontSize: 14.sp),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 48.dp),
+            ],
+          ),
+        ),
       ),
     );
   }
