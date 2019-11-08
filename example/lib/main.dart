300227a0e272b6f37d0b9c8fd9e243d8cd99d0be
Jogboms <jeremiahogbomo@gmail.com>
2019-11-08 15:27:37 +0100

    Example code generation

---
+++ example/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Demo',
+      theme: ThemeData.dark(),
+      home: MyHomePage(title: 'Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Center(
+        child: Text('You have pushed the button this many times:'),
+      ),
+    );
+  }
+}
